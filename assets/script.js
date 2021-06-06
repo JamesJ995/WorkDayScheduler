f0be5e20b3cba23e12f76a2f892368ab6f18731d
jamesj995 <mrjam7@gmail.com>
2021-06-06 04:07:16 -0500

    added comments and cleaned up file structure

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var currentDate = moment().format("Do of MMMM, YYYY");
+var currentDate = moment().format("Do of MMMM, YYYY, ka");
 var hour9 = $("#time9am");
 var hour10 = $("#time10am");
 var hour11 = $("#time11am");
@@ -20,6 +20,7 @@ var timeBlocks = [
   hour5,
 ];
 
+//get local storage items and set timeblock text to their values.
 function populateLocalStorage() {
   $("#time9am .description").val(localStorage.getItem("time9am"));
   $("#time10am .description").val(localStorage.getItem("time10am"));
@@ -32,25 +33,29 @@ function populateLocalStorage() {
   $("#time5pm .description").val(localStorage.getItem("time5pm"));
 }
 
+//render the "past present or future" color coding according to the hour of the day when the page is loaded.
 function renderTime() {
   var currentHour = moment().format("ka");
   console.log(currentHour);
 
+  //loop through each time block
   for (var i = 0; i < timeBlocks.length; i++) {
     var loopBlock = timeBlocks[i].attr("id");
     loopBlock = loopBlock.slice(4);
 
+    //some issue with the value "9am" required this workaround.
     if (currentHour === "9am") {
       timeBlocks[0].removeClass("future");
       timeBlocks[0].removeClass("past");
       timeBlocks[0].addClass("present");
-      for (var i = 1; i < timeBlocks.length; i++){
+      for (var i = 1; i < timeBlocks.length; i++) {
         timeBlocks[i].removeClass("past");
         timeBlocks[i].removeClass("present");
         timeBlocks[i].addClass("future");
       }
       return;
     }
+    //conditions for past
     if (loopBlock < currentHour) {
       timeBlocks[i].removeClass("future");
       timeBlocks[i].removeClass("present");
@@ -59,6 +64,7 @@ function renderTime() {
       timeBlocks[0].removeClass("present");
       timeBlocks[0].addClass("past");
     }
+    //condition for future
     if (loopBlock > currentHour) {
       timeBlocks[i].removeClass("past");
       timeBlocks[i].removeClass("present");
@@ -67,6 +73,7 @@ function renderTime() {
       timeBlocks[0].removeClass("present");
       timeBlocks[0].addClass("past");
     }
+    //condition for present
     if (loopBlock === currentHour) {
       timeBlocks[i].removeClass("future");
       timeBlocks[i].removeClass("present");
@@ -78,6 +85,7 @@ function renderTime() {
   }
 }
 
+//event listener to save textarea entry when the save button is clicked
 $(".saveBtn").on("click", function () {
   var userEntry = $(this).siblings(".description").val();
   var time = $(this).parent().attr("id");
@@ -85,6 +93,7 @@ $(".saveBtn").on("click", function () {
   localStorage.setItem(time, userEntry);
 });
 
+//initialize page, disply current date and time, and run functions.
 $("#currentDay").html(currentDate);
 populateLocalStorage();
 renderTime();
